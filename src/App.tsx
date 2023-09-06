370212a089950b999388272024feded339bc432f
Lucas Magnusson <lucasmyeet@gmail.com>
2023-09-06 15:52:18 +0200

    updated loginpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import LandingPage from './pages/landing/LandingPage';
+import { UserInfo } from './types/UserTypes';
 
+const defaultUser: UserInfo = {
+  username: "",
+  password: "",
+  role: "USER",
+}
 
 
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(defaultUser) 
+
+  
   return (
     <>
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<LandingPage/>}></Route>
+          <Route path="/" element={<LandingPage setCurrentUser={setCurrentUser}/>} />
         </Routes>
       </div>
     </Router>
--- src/pages/logIn/LoginPage.tsx
+++ src/pages/logIn/LoginPage.tsx
@@ -1,4 +1,10 @@
-export default function LoginPage () {
+import { UserInfo } from "../../types/UserTypes"
+
+type UserProps = {
+    setCurrentUser: React.Dispatch<React.SetStateAction<UserInfo>>
+}
+
+export default function LoginPage (User: UserProps) {
     return (
         <></>
     )
