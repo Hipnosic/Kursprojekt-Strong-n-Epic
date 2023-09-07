c7e6f2417453b5528ea115005b50b57bd4afc40d
ledei <holybuzz@live.se>
2023-09-07 10:50:14 +0200

    created form with a input handler,cancel handler and a submit handler with out a logic

--- src/pages/logIn/LoginPage.tsx
+++ src/pages/logIn/LoginPage.tsx
@@ -1,11 +1,63 @@
-import { UserInfo } from "../../types/UserTypes"
+import { useState } from "react";
+import { UserInfo } from "../../types/UserTypes";
+import { Link } from "react-router-dom";
 
-type UserProps = {
-    setCurrentUser: React.Dispatch<React.SetStateAction<UserInfo>>
+interface userDetails {
+  username: string;
+  password: string;
 }
 
-export default function LoginPage (User: UserProps) {
-    return (
-        <></>
-    )
+type LoginPageProps = {
+  setCurrentUser: React.Dispatch<React.SetStateAction<UserInfo>>;
 };
+
+const LoginPage: React.FC<LoginPageProps> = ({ setCurrentUser }) => {
+  const [credential, setCredential] = useState<userDetails>({ username: "", password: "" });
+
+  const handleCredential = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCredential({ ...credential, [e.target.name]: e.target.value });
+  };
+
+  const handleCancelBtn = () => {
+    setCredential({ username: "", password: "" });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Användarnamn</label>
+        <input
+          name="username"
+          type="text"
+          onChange={(e) => {
+            handleCredential(e);
+          }}
+        />
+      </div>
+      <div>
+        <label>Lössenord</label>
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => {
+            handleCredential(e);
+          }}
+        />
+      </div>
+
+      <button type="submit">Login</button>
+      <button type="reset" onClick={handleCancelBtn}>
+        Cancel
+      </button>
+      <p>
+        No account? <Link to="signup">Click here</Link>
+      </p>
+    </form>
+  );
+};
+
+export default LoginPage;
