72447ab0b700b792f584348f76d19fc108260683
Lucas Magnusson <lucasmyeet@gmail.com>
2023-09-07 10:38:53 +0200

    added server file and loginpage route

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import LandingPage from "./pages/landing/LandingPage";
-import { UserInfo } from "./types/UserTypes";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import LandingPage from './pages/landing/LandingPage';
+import LoginPage from './pages/logIn/LoginPage';
+import { UserInfo } from './types/UserTypes';
 
 const defaultUser: UserInfo = {
   username: "",
   password: "",
   role: "USER",
-};
+}
 
 function App() {
   const [currentUser, setCurrentUser] = useState(defaultUser);
+
+  useEffect(() => {
+    fetch('/users')
+      .then((response) => {
+        return response.json
+      })
+  })
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<LandingPage setCurrentUser={setCurrentUser} />} />
+          <Route path="/" element={<LandingPage/>} />
+          <Route path="/login" element={<LoginPage setCurrentUser={setCurrentUser}/>} />
         </Routes>
       </Router>
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,44 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { UserInfo } from './types/UserTypes';
+import { Server } from "miragejs"
+
+let userArray: UserInfo[] = [
+  { 
+    username: "Hotdog56",
+    password: "123",
+    role: "ADMIN",
+  },
+  { 
+    username: "Arnoldfan123",
+    password: "123",
+    role: "USER",
+  },
+  { 
+    username: "Jacke",
+    password: "123",
+    role: "USER",
+  },
+]
+
+new Server({
+  routes() {
+    this.namespace = "api"
+
+    this.get("/users", () => {
+      return { users: userArray }
+    })
+
+    this.post("/users", (schema, request) => {
+      let body = JSON.parse(request.requestBody)
+      userArray.push(body)
+
+      return { users: body };
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
