3387b3add37f092f6f1e4485469e398ad3c29921
ledei <holybuzz@live.se>
2023-09-07 10:26:52 +0200

    added a new type and added sessitem that is a tamplate how to render sessions and made a session list that renders the list

---
+++ src/components/SessionList.tsx
@@ -0,0 +1,18 @@
+import { Session } from "../types/Session";
+import SessionItem from "./SessionItem";
+
+interface SessionListProps {
+  sessions: Session[] | undefined;
+}
+
+const SessionList: React.FC<SessionListProps> = ({ sessions }) => {
+  return (
+    <div>
+      {sessions?.map((session, i) => (
+        <SessionItem key={i} session={session} />
+      ))}
+    </div>
+  );
+};
+
+export default SessionList;
