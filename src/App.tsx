be943525e862f634d46440eb3ca9c909e6852c31
ledei <holybuzz@live.se>
2023-09-07 10:16:42 +0200

    deleted the test

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import LandingPage from './pages/landing/LandingPage';
-import { UserInfo } from './types/UserTypes';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import LandingPage from "./pages/landing/LandingPage";
+import { UserInfo } from "./types/UserTypes";
 
 const defaultUser: UserInfo = {
   username: "",
   password: "",
   role: "USER",
-}
-
-
+};
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(defaultUser) 
+  const [currentUser, setCurrentUser] = useState(defaultUser);
 
-  
   return (
     <>
-    <Router>
-      <div className='App'>
+      <Router>
         <Routes>
-          <Route path="/" element={<LandingPage setCurrentUser={setCurrentUser}/>} />
+          <Route path="/" element={<LandingPage setCurrentUser={setCurrentUser} />} />
         </Routes>
-      </div>
-    </Router>
+      </Router>
     </>
   );
 }
