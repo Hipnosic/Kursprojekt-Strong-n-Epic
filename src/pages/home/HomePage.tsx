f23bcbd32462ed0d2644b4dc6eac27d6c4902cba
Daniel Rotaru <daniel_rotaru@hotmail.com>
2023-09-08 16:15:50 +0200

    homepage update

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -11,13 +11,47 @@ const HomePage: React.FC<HomePageProps> = ({ setCurrentSession }) => {
   const [dateSearch, setDateSearch] = useState<string>("");
   const { isLoading, error, data } = useFetchSession(dateSearch);
 
+  
+  const [showSchedule, setShowSchedule] = useState<boolean>(false);
+
+ 
+  const toggleSchedule = () => {
+    setShowSchedule(!showSchedule);
+  };
+
   return (
     <>
-      <input type="date" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setDateSearch(e.target.value)} />
-      <button onClick={() => setDateSearch("")}>Clear Filter</button>
-      {(error && <p>404 could not found</p>) ||
-        (isLoading && <p>loading...</p>) ||
-        (data?.length === 0 && <p>There is no session on {dateSearch}</p>) || <SessionList sessions={data} />}
+      <div className="menu">
+        <button className="menu-home-btn">Home</button>
+        <button className="menu-admin-btn">Admin</button>
+      </div>
+      <div>
+        <div className="nav-btns">
+          <button
+            className={`nav-schedule-btn${showSchedule ? " active" : ""}`}
+            onClick={toggleSchedule}
+          >
+            Schedule
+          </button>
+          <button className="nav-bookings-btn">My Bookings</button>
+        </div>
+        {showSchedule && (
+          <>
+            <input
+              type="date"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setDateSearch(e.target.value)
+              }
+            />
+            <button onClick={() => setDateSearch("")}>Clear Filter</button>
+            {(error && <p>404 could not found</p>) ||
+              (isLoading && <p>loading...</p>) ||
+              (data?.length === 0 && (
+                <p>There is no session on {dateSearch}</p>
+              )) || <SessionList sessions={data} />}
+          </>
+        )}
+      </div>
     </>
   );
 };
