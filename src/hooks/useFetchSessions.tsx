5ea3acc8efae8ca97ad1b01ad8f0b032563ab568
ledei <holybuzz@live.se>
2023-09-08 09:59:15 +0200

    documented useFetchSessions

--- src/hooks/useFetchSessions.tsx
+++ src/hooks/useFetchSessions.tsx
@@ -2,6 +2,11 @@ import { useState, useEffect } from "react";
 import { Session } from "../types/Session";
 import requestService from "../service/requestService";
 
+/**
+ * useFetchSession is a custom hook to fetch all the sessions in the server and sends out when its loading or when there is an error and if everything is fine sends the data out
+ * @returns isLoading, error, data
+ */
+
 const useFetchSession = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<boolean>(false);
